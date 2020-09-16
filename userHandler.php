ddcdfbcc2c4c10f90b57bace0f5d8a3f015cd30a
MrDuise <53163559+MrDuise@users.noreply.github.com>
2020-09-16 13:10:21 -0700

    Update userHandler.php

--- userHandler.php
+++ userHandler.php
@@ -1,7 +1,14 @@
+//Michael Duisenberg
+//CST-126
+//09-12-20
+//This page handles the form gathering and inserts the data in the database
+
+//html was used around the PHP code in order to add CSS styling. 
 <html>
 	<head>
-		<title> Welcome to the Blog</title>
+		<title> Registration Complete</title>
 		<style>
+		/* centers output on screen */
 				p{
 					height: 10em;
 					position: relative;
@@ -21,22 +28,20 @@
 				background-color: grey;
 				}
 		
-		
-		
-		
 		</style>
 		
 	</head>
 	<body>
 		<p>
 		<?php
+		//variables are set to the value of the data entered in the form
 			$personFirst = addslashes($_POST["firstName"]);
 			$personLast = addslashes($_POST["lastName"]);
 			$userName = $_POST["userName"];
 			$email = $_POST["email"];
 			$passWord = $_POST["passWord"];
 		?>
-
+			
 			<?php echo "You are now registered."; ?> <br>
 			<?php echo "First Name is " . $personFirst;?> <br>
 			<?php echo "Last Name is " . $personLast;?> <br>
@@ -50,10 +55,10 @@
 				$port = 8889;
 
 
-				// Create connection
+				// Create connection to the database
 				$conn = mysqli_connect($host, $username, $password, $database_name, $port);
 
-				// Check connection
+				// Check connection to database and inform user if it either connected successfully or failed
 				if (!$conn) {
 					die("Connection failed: " . mysqli_connect_error());
 				}
@@ -63,7 +68,7 @@
 
 				$sql = "INSERT INTO `users` (`User_Name`, `First_Name`, `Last_Name`, `Email`, `Password`) VALUES ('$userName', '$personFirst', '$personLast', '$email', '$passWord')";
 
-
+                //informs the user that they are now entered into the system or spits out an error if something went wrong
 				if (mysqli_query($conn, $sql)) {
 					echo "New record created successfully";
 					} else {
