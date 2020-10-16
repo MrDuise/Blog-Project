42d7864851eb94d9c1bd8be30eadffa769d4eca0
MrDuise <53163559+MrDuise@users.noreply.github.com>
2020-10-16 14:31:11 -0700

    Update viewPost.php

--- viewPost.php
+++ viewPost.php
@@ -11,50 +11,51 @@ require_once 'myfuncs.php';
 
 <!DOCTYPE html>
 <html>
-<head>
-<link rel="icon" href="letter-b-64v-prints.png">
-<title>  Welcome to my blog </title>
-<link rel="stylesheet" href="welcomeStyle.css">
+	<head>
+	<link rel="icon" href="letter-b-64v-prints.png">
+	<title>  Welcome to my blog </title>
+	<link rel="stylesheet" href="welcomeStyle.css">
 
 
 
 
-<meta charset="ISO-8859-1">
+	<meta charset="ISO-8859-1">
 
-</head>
-<body>
-<h1>User <?php echo $_SESSION['username']; ?> Entries </h1>
-<div class="topnav"> 
-	<p> Welcome <?php echo $_SESSION['username']; ?> </p>
-	<a href="addPost.php">Add Post</a>
- 	<a href="logOut.php">Logout</a> 
- 	<a href="main-welcome.php">Home</a>
- 	</div>
-<p>
-<?php 
-
-
-$sql = "SELECT * FROM `user_posts`";
+	</head>
+	<body>
+		<h1>User <?php echo $_SESSION['username']; ?> Entries </h1>
+		<div class="topnav"> 
+			<p> Welcome <?php echo $_SESSION['username']; ?> </p>
+			<a href="addPost.php">Add Post</a>
+ 			<a href="logOut.php">Logout</a> 
+ 			<a href="main-welcome.php">Home</a>
+ 		</div>
+	<p>
+		<?php 
 
 
-if(dbConnect())
-{
-    $result = mysqli_query(dbConnect(),$sql);
+            $sql = "SELECT * FROM `user_posts`";
+
+
+            if(dbConnect())
+            {
+              $result = mysqli_query(dbConnect(),$sql);
     
-    if($result)
-    {
-        while($row = mysqli_fetch_assoc($result))
-        {
-            echo "Post ID: " . $row['ID'] . "<br>";
-            echo "Post Title: " . $row['Blog_Title'] . "<br>";
-            echo "Post Entry: " . $row['Blog_Entry'] . "<br>";
-            echo "===========<br>";
-        }
-    }    
-}
-?>
-</p>
+                if($result)
+                {
+                     while($row = mysqli_fetch_assoc($result))
+                    {
+                     echo "Post ID: " . $row['ID'] . "<br>";
+                     echo "Post Title: " . $row['Blog_Title'] . "<br>";
+                     echo "Post Entry: " . $row['Blog_Entry'] . "<br>";
+                     echo "===========<br>";
+                    }
+                 }    
+            }
+        ?>
+	</p>
 
-<p><?php include 'footer.php' ?> </p>
-</body>
+	<p><?php include 'footer.php' ?> </p>
+	</body>
+</html>
 </html>
