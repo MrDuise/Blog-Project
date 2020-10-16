b6316f2c32fb8ec09b344cbf00406f1f6cfa4413
MrDuise <53163559+MrDuise@users.noreply.github.com>
2020-10-16 14:32:08 -0700

    Update index.php

--- index.php
+++ index.php
@@ -10,7 +10,23 @@
 	<link rel="icon" href="letter-b-64v-prints.png">
 	<title>  Welcome to my blog </title>
 	<link rel="stylesheet" href="welcomeStyle.css">
-	
+	<style>
+		/* centers output on screen */
+				.middle p{
+					height: 6em;
+					position: relative;
+					margin: 0;
+					position: absolute;
+					top: 50%;
+					left: 50%;
+					margin-right: -50%;
+					transform: translate(-50%, -50%);
+					text-align: center;
+					background-color:grey;
+					width:400px;
+					border:1px solid black;
+				}
+		</style>
 	
 	
 		
@@ -27,11 +43,13 @@
 		<a href= "userLogin.php">Login</a>
 		</div>
 		
+		<div class="middle">
+		
 		<p>We all drive, but sometimes driving can get a little crazy<br>
 			This Blog is pretty much twitter, but just for those crazy times<br>
 			you wanted to drive someone off a cliff.
-		
 		</p>
+		</div>
 		
 		
 		<p><?php include 'footer.php' ?> </p>
