e64866b06aca98581b30460e9ae5655c714b1862
MrDuise <53163559+MrDuise@users.noreply.github.com>
2020-11-10 08:51:28 -0700

    Update viewPost.php

--- viewPost.php
+++ viewPost.php
@@ -3,8 +3,8 @@
 //10-18-20
 //CST-126
 //MileStone 3 View Post page
-//This pages displays all of the users posts
-//Version 1.0
+//This pages displays all of the users posts titles with a button that links to the rest of the post
+//Version 2.0
 session_start();
 require_once 'myfuncs.php';
 ?>
@@ -30,7 +30,10 @@ require_once 'myfuncs.php';
 
 		$id = $_SESSION['userid'];
             $sql = "SELECT * FROM `user_posts` WHERE `users_ID` = '$id'";
-
+            $sql_user = "SELECT * FROM `users` WHERE `ID` = '$id'";//select statement for getting the username out of the database
+            $result_userName = mysqli_query(dbConnect(), $sql_user);
+            $row_user = mysqli_fetch_assoc($result_userName);
+            $userName = $row_user['User_Name'];
 
             if(dbConnect())
             {
@@ -43,12 +46,19 @@ require_once 'myfuncs.php';
                      echo "Post ID: " . $row['ID'] . "<br>";
                      echo "Post Title: " . $row['Blog_Title']. " ";
                      
+                    
+                         
+                             
+                         
+                 
+                     
                      ?>
          </p>
          
                     
                      <form action = "viewPostContent.php">
             		<input type="hidden" name="id" value = "<?php echo $row['ID'];?>"></input>
+            		<input type="hidden" name="userName" value = "<?php echo $userName;?>"></input> 
            			 <button type="submit">View</button>
            			 </form>
                      
